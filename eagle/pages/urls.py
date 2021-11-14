f3bae6ad596f8219f4e4f915ab4bed610a1b6ca1
Nick <nick.smith.tsa@gmail.com>
2021-11-14 14:58:56 +0000

    pages api viewset

--- eagle/pages/urls.py
+++ eagle/pages/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path("", views.get_routes, name="routes"),
-    path("pages", views.get_pages, name="get_pages"),
-]
+router = DefaultRouter()
+router.register(r"pages", views.PagesViewSet, basename="page")
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path("", views.get_routes, name="routes"),
+#     path("pages", views.pages, name="pages"),
+#     # path("page/<pk>", views.get_page, name="get_page"),
+# ]
--- eagle/pages/views.py
+++ eagle/pages/views.py
@@ -1,22 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from .serializers import PageSerializer
 from .models import Page
 
 
-@api_view(["GET"])  # can also pass in others e.g. PUT, POST, etc
-def get_routes(request):
-    routes = [
-        {"Endpoint": "/test/", "method": "GET", "body": None, "description": "Test"},
-    ]
-    return Response(routes)
+class PagesViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Page.objects.all().order_by("-date_created")
+        serializer = PageSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Page.objects.all()
+        page = get_object_or_404(queryset, pk=pk)
+        serializer = PageSerializer(page)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
 
 
-@api_view(["GET"])
-def get_pages(request):
-    pages = Page.objects.all()
-    serializer = PageSerializer(
-        pages, many=True
-    )  # many = serialise many objects; return queryset
-    return Response(serializer.data)
+# https://www.django-rest-framework.org/api-guide/viewsets/
+
+# old way using functions
+# from rest_framework.decorators import api_view
+# @api_view(["GET"])  # can also pass in others e.g. PUT, POST, etc
+# def get_routes(request):
+#     routes = [
+#         {
+#             "Endpoint": "/pages/",
+#             "method": "GET",
+#             "body": None,
+#             "description": "Fetches pages",
+#         },
+#     ]
+#     return Response(routes)
+
+
+# @api_view(["GET", "POST", "PUT", "DELETE"])
+# def pages(request):
+#     if request.method == "GET":
+#         pages = Page.objects.all().order_by("-date_created")
+#         serializer = PageSerializer(
+#             pages, many=True
+#         )  # many = serialise many objects; return queryset
+#         return Response(serializer.data)
+
+#     if request.method == "POST":
+#         data = request.data
+#         page = Page.objects.create(body=data["body"])
+#         serializer = PageSerializer(page, many=False)
+#         return Response(serializer.data)
+
+
+# @api_view(["GET"])
+# def get_pages(request):
+#     pages = Page.objects.all().order_by("-date_created")
+#     serializer = PageSerializer(
+#         pages, many=True
+#     )  # many = serialise many objects; return queryset
+#     return Response(serializer.data)
+
+
+# @api_view(["GET"])
+# def get_page(request, pk):
+#     pages = Page.objects.get(id=pk)
+#     serializer = PageSerializer(pages, many=False)
+#     return Response(serializer.data)
+
+
+# if request.method == 'GET':
+# if request.method == 'POST':
