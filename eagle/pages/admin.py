8048ed9417e1dd1d83f122c89a8d147813d8d8a4
Nick <nick.smith.tsa@gmail.com>
2021-11-14 11:23:02 +0000

    page and keyword models

--- eagle/pages/admin.py
+++ eagle/pages/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Page
+from .models import Page, Keyword
 
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
-    list_display = ("notes",)
+    list_display = (
+        "title",
+        "type",
+        "date_created",
+    )
+    readonly_fields = ("date_created",)  # shows date_created on individual 'page' pages
+
+
+@admin.register(Keyword)
+class PageAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "page",
+    )
--- eagle/pages/models.py
+++ eagle/pages/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+PAGE_TYPES = [("landing", "landing"), ("sale", "sale"), ("other", "other")]
+
 
 class Page(models.Model):
-    notes = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=100, null=True, blank=False)
+    type = models.CharField(max_length=100, choices=PAGE_TYPES, null=True, blank=False)
+    slug = models.CharField(max_length=200, null=True, blank=True)
+    notes = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=False)
+    page = models.ForeignKey(Page, on_delete=models.CASCADE, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
