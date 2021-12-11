a2f1bd080c0fcc3099673b3576422ba144cfc448
Nick <nick.smith.tsa@gmail.com>
2021-12-11 22:22:18 +0000

    Ensure array of objects has id key

--- frontend/src/utils/HelperFunctions.tsx
+++ frontend/src/utils/HelperFunctions.tsx
@@ -1,5 +1,3 @@
-import { strictEqual } from 'assert';
-
 export const formatDate = (date: string): string => {
     const actualDate = new Date(date);
     const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(actualDate);
@@ -11,11 +9,7 @@ export const formatDate = (date: string): string => {
     return `${day}/${month}/${year}`;
 };
 
-// @todo: create type that makes function invalid if the arr passed into it doesn't have a {id} key (that's also a number)
-// ^ or instead throw an error?
-// see https://stackoverflow.com/questions/70282562/type-for-any-array-that-has-an-id-key-with-number-value
-// https://www.typescriptlang.org/docs/handbook/2/generics.html#generic-constraints
-export const removeItemFromArray = (id: number, arr: Array<any>): Array<any> => {
+export const removeItemFromArray = (id: number, arr: Array<{ id: number }>): Array<{}> => {
     const indexToRemove: number = arr.map((el) => el.id).indexOf(id);
     ~indexToRemove && arr.splice(indexToRemove, 1); // ~ = >= 0
     return arr;
