9010fc1d14b484286455310b17656077e66ac840
Nick <nick.smith.tsa@gmail.com>
2021-12-20 21:20:10 +0000

    Mobile navmenu toggle display state

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,44 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import headerStyles from '../css/components/header.module.scss';
 import { Link } from 'react-router-dom';
 import { Section } from './Default';
 import defaultStyles from '../css/default.module.scss';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight, faBars, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface HeaderProps {
     cls?: string;
 }
+
+export const Header: FC<HeaderProps> = ({ cls = '' }) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <Section clsOuter={`${headerStyles.headerOuter} ${cls}`} clsInner={headerStyles.headerInner} tag='header'>
+            <h2>
+                <Link to='/'>SEOTHING</Link>
+            </h2>
+            <div className={headerStyles.rightArea}>
+                <FontAwesomeIcon icon={menuOpen ? faTimesCircle : faBars} onClick={() => setMenuOpen(!menuOpen)} />
+                <div className={`${headerStyles.rightAreaInner} ${menuOpen ? headerStyles.open : ''}`}>
+                    <ul>
+                        {links.map((link) => (
+                            <li key={link.title}>
+                                <Link to={link.url}>
+                                    {link.title}
+                                    <FontAwesomeIcon icon={faAngleRight} />
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <Link to='/login' className={defaultStyles.btnSecondary}>
+                        Login
+                    </Link>
+                </div>
+            </div>
+        </Section>
+    );
+};
 
 const links = [
     {
@@ -24,26 +54,3 @@ const links = [
         url: '/pages',
     },
 ];
-
-export const Header: FC<HeaderProps> = ({ cls = '' }) => (
-    <Section clsOuter={`${headerStyles.headerOuter} ${cls}`} clsInner={headerStyles.headerInner} tag='header'>
-        <h2>
-            <Link to='/'>SEOTHING</Link>
-        </h2>
-        <div className={headerStyles.rightArea}>
-            <FontAwesomeIcon icon={faBars} />
-            <div className={headerStyles.rightAreaInner}>
-                <ul>
-                    {links.map((link) => (
-                        <li key={link.title}>
-                            <Link to={link.url}>{link.title}</Link>
-                        </li>
-                    ))}
-                </ul>
-                <Link to='/login' className={defaultStyles.btnSecondary}>
-                    Login
-                </Link>
-            </div>
-        </div>
-    </Section>
-);
